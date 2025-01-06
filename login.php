61ce0993550a8140f7e07de689a4e829c1b1746e
SamhainV <antoniomartinezramirez@gmail.com>
2025-01-06 19:39:37 +0100

    1

--- login.php
+++ login.php
@@ -3,8 +3,8 @@ session_start();
 require_once 'config/database.php'; // Asegúrate de tener acceso a la conexión de la base de datos.
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
+    $login_email = $_POST['login_email'] ?? '';
+    $login_password = $_POST['login_password'] ?? '';
 
     // Conexión a la base de datos
     $db = new Database();
@@ -13,13 +13,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Buscar el usuario por email
     $sql = "SELECT * FROM employee WHERE email = :email";
     $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':email', $email);
+    $stmt->bindValue(':email', $login_email);
     $stmt->execute();
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     // Verificar si el usuario existe y la contraseña es correcta
-    if ($user && password_verify($password, $user['password'])) {
+    if ($user && password_verify($login_password, $user['password'])) {
         // Crear la sesión del usuario
         $_SESSION['user'] = [
             'email' => $user['email'],
@@ -33,6 +33,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -44,12 +49,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php if (!empty($error)): ?>
         <p style="color:red;"><?= htmlspecialchars($error) ?></p>
     <?php endif; ?>
-    <form method="POST" action="login.php" autocomplete="off">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" placeholder="usuario@ejemplo.com" required>
+    <form method="POST" action="login.php">
+        <!-- Campo oculto para prevenir autocompletar nombres de usuario -->
+        <input type="text" name="fake_username" id="fake_username" style="display: none;" autocomplete="username">
+
+        <label for="login_email">Email:</label>
+        <input type="email" name="login_email" id="login_email" placeholder="usuario@ejemplo.com" required autocomplete="email">
         <br><br>
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" required>
+        <label for="login_password">Contraseña:</label>
+        <input type="password" name="login_password" id="login_password" required autocomplete="current-password">
         <br><br>
         <button type="submit">Iniciar Sesión</button>
     </form>
