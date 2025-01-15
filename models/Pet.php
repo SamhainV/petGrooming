120d138c284add45d5b02fac86e4f06ad4152e77
SamhainV <antoniomartinezramirez@gmail.com>
2025-01-15 21:07:19 +0100

    1

--- models/Pet.php
+++ models/Pet.php
@@ -12,10 +12,10 @@ class Pet {
     private $pdo;
 
     public function __construct() {
-        $db = new Database();
-        $this->pdo = $db->getConnection();
-    }
-
+        $db = Database::getInstance(); // Utilizar el método estático
+        $this->pdo = $db->getConnection(); // Obtener la conexión PDO
+    }    
+    
     /**
      * Obtiene todos los registros de la tabla pet, incluyendo el nombre completo del dueño.
      */
